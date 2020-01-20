dc608e170b1127d56b20e1dcdc789808a7cd794d
shaik.nasurulla@eidiko.com <shaik.nasurulla@eidiko.com>
2020-01-20 12:56:23 +0530

    PROJECT

--- Build/Deploy.bat
+++ Build/Deploy.bat
@@ -1,1 +1,1 @@
-C:\"ProgramFiles"\IBM\ACE\11.0.0.7\server\bin\mqsiprofile.cmd && C:\"Program Files"\IBM\ACE\11.0.0.7\server\bin\mqsideploy TEST -e IS -a rp.bar
+C:\"ProgramFiles"\IBM\ACE\11.0.0.7\server\bin\mqsiprofile.cmd && C:\"Program Files"\IBM\ACE\11.0.0.7\server\bin\mqsideploy -n TEST -e IS -a rp.bar -m
